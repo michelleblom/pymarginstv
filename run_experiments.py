6865da3bd6b492da35c665da4c3e5da71b3ae119
Alexander Ek <djalekzse@gmail.com>
2024-04-02 17:11:28 +1100

    fixed number of experiments

--- run_experiments.py
+++ run_experiments.py
@@ -13,16 +13,16 @@ datafiles = [
     ("Scotland/GCC_07_Linn_ballots.txt", 4), ("Scotland/GCC_07_Maryhill_ballots.txt", 4),
     ("Scotland/GCC_07_Newlands_ballots.txt", 3), ("Scotland/GCC_07_NorthEast_ballots.txt", 4),
     ("Scotland/GCC_07_Partick_ballots.txt", 4), ("Scotland/GCC_07_Pollockshields_ballots.txt", 3),
-    ("Scotland/GCC_07_Shettleston_ballots.txt", 4), ("Scotland/GCC_07_SouthsideCentral_ballots.txt", 4),
-    ("Scotland/GCC_07_Springburn_ballots.txt", 3),
+    #("Scotland/GCC_07_Shettleston_ballots.txt", 4), ("Scotland/GCC_07_SouthsideCentral_ballots.txt", 4),
+    #("Scotland/GCC_07_Springburn_ballots.txt", 3),
     # AUSTRALIA 2016, 2019, 2022
-    ("FedAus/federal_2016_ACT.stv", 2), ("FedAus/federal_2016_NT.stv", 2),
-    ("FedAus/federal_2019_ACT.stv", 2), ("FedAus/federal_2019_NT.stv", 2),
+    #("FedAus/federal_2016_ACT.stv", 2), ("FedAus/federal_2016_NT.stv", 2),
+    #("FedAus/federal_2019_ACT.stv", 2), ("FedAus/federal_2019_NT.stv", 2),
     ("FedAus22/2022NT.json", 2), ("FedAus22/2022ACT.json", 2)
 ]
 
 
-reps = 5
+reps = 3
 counter = 0
 
 
